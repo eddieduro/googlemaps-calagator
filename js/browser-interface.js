e8f7ca16f0c7fb6c04b8dc3e7f7cc328d3654f7b
eddie duro and michael vang <michael@>
2016-03-17 14:36:21 -0700

    add styling to site

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -13,27 +13,28 @@ $( document ).ready(function() {
     type: "GET",
     dataType: 'JSONP',
     crossDomain: true,
-    url: "https://api.meetup.com/find/groups?callback=?&key=" + meetUpApiKey +"&photo-host=public&lon=" + longitude + "&text=web development&lat=" + latitude + "&page=80&sign=true"
+    url: "https://api.meetup.com/find/groups?callback=?&key=" + meetUpApiKey +"&photo-host=public&lon=" + longitude + "&text=web development&lat=" + latitude + "&page=2&sign=true"
     }).then(function (response) {
       $.each(response, function (i, items) {
+        console.log(i, items)
         var markers = defaultPosition(items);
-        $.each(items, function(j, item) {
-          var nextEvent = item.next_event;
-          if( nextEvent ){
-            var eventId = item.next_event.id;
-            // $.get("https://api.meetup.com/2/events?&sign=true&photo-host=public&event_id="+ eventId +"&page=20", function(data){
-            //   console.log(data);
-            // });
-            $.ajax({
-            type: "GET",
-            dataType: 'JSONP',
-            crossDomain: true,
-            url: "https://api.meetup.com/2/events?&key="+ meetUpApiKey +"&sign=true&photo-host=public&event_id="+ eventId +"&page=20"
-          }).then(function (j, data) {
-            console.log(j, data);
-          });
-        }
-        });
+        // $.each(items, function(j, item) {
+        //   var nextEvent = item.next_event;
+        //   if( nextEvent ){
+        //     var eventId = item.next_event.id;
+        //     // $.get("https://api.meetup.com/2/events?&sign=true&photo-host=public&event_id="+ eventId +"&page=20", function(data){
+        //     //   console.log(data);
+        //     // });
+        //     $.ajax({
+        //     type: "GET",
+        //     dataType: 'JSONP',
+        //     crossDomain: true,
+        //     url: "https://api.meetup.com/2/events?&key="+ meetUpApiKey +"&sign=true&photo-host=public&event_id="+ eventId +"&page=20"
+        //   }).then(function (k, data) {
+        //      console.log(data.results[0].event_url);
+        //   });
+        // }
+        // });
     });
   });
 });
