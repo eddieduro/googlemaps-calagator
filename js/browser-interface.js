c1ee46ebab3e8f4558df2767358c5511981b0d36
eddie duro and michael vang <eddie@>
2016-03-17 15:26:40 -0700

    add current events

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -7,6 +7,7 @@ var serverKey = require('./../.env').serverKey;
 $( document ).ready(function() {
   defaultPosition();
   markersArray = [];
+  eventArray = [];
     var latitude = 45.5189614;
     var longitude = -122.6865243;
     $.ajax({
@@ -16,25 +17,35 @@ $( document ).ready(function() {
     url: "https://api.meetup.com/find/groups?callback=?&key=" + meetUpApiKey +"&photo-host=public&lon=" + longitude + "&text=web development&lat=" + latitude + "&page=2&sign=true"
     }).then(function (response) {
       $.each(response, function (i, items) {
-        console.log(i, items)
+        // console.log(i, items)
         var markers = defaultPosition(items);
-        // $.each(items, function(j, item) {
-        //   var nextEvent = item.next_event;
-        //   if( nextEvent ){
-        //     var eventId = item.next_event.id;
-        //     // $.get("https://api.meetup.com/2/events?&sign=true&photo-host=public&event_id="+ eventId +"&page=20", function(data){
-        //     //   console.log(data);
-        //     // });
-        //     $.ajax({
-        //     type: "GET",
-        //     dataType: 'JSONP',
-        //     crossDomain: true,
-        //     url: "https://api.meetup.com/2/events?&key="+ meetUpApiKey +"&sign=true&photo-host=public&event_id="+ eventId +"&page=20"
-        //   }).then(function (k, data) {
-        //      console.log(data.results[0].event_url);
-        //   });
-        // }
-        // });
+        $.each(items, function(j, item) {
+          // console.log(item);
+          var nextEvent = item.next_event;
+          if( nextEvent ){
+            var eventId = item.next_event.id;
+            $.ajax({
+            type: "GET",
+            dataType: 'JSONP',
+            crossDomain: true,
+            url: "https://api.meetup.com/2/events?&key="+ meetUpApiKey +"&sign=true&photo-host=public&event_id="+ eventId +"&page=2"
+          }).then(function (data) {
+            var event_url = data.results[0].event_url;
+            var eventName = data.results[0].name;
+            var eventAddress = data.results[0].venue.address_1;
+            var eventCity = data.results[0].venue.city;
+            var eventState = data.results[0].venue.state;
+            var eventLat = data.results[0].venue.lat;
+            var eventLon = data.results[0].venue.lon;
+            console.log(data);
+            // $.each(data, function(k, log) {
+            //   var eventAddress =
+            //   console.log(log);
+            // })
+            $("#current").append("<h4>"+ eventName + "</h4><p>" + eventAddress + " " + eventCity + ", " + eventState + "<li><a target='_blank' href='"+ event_url +"'>" + event_url + "</li>");
+          });
+        }
+        });
     });
   });
 });
